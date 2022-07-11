3775aa6c470d13d99af5c667d3acac16667bf907
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-07-11 03:16:39 -0700

    Mainheader ammend

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * _putchar - writes the character c to stdout
--- main.h
+++ main.h
@@ -1,1 +1,5 @@
+#ifndef
+#define
 int _printf(const char *format, ...);
+int _print_string(char *s);
+#endif

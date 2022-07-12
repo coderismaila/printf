49b7f7776f623209ba239735b5a33d1caa4a1773
Ismaila Hassan <code.ismailah28@gmail.com>
2022-07-12 11:54:54 -0700

    add function prototypes

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 #define NULL ((void *)0)
 
 void _putchar(char c);
 int _printf(const char *format, ...);
+int _print_str(char *str);
+int _print_int(long int num);
+int convert(const char *fmt, va_list list);
 
 #endif

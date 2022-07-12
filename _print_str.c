58b535f6c6dcfd970d3d8c3c8eed830ec82f379c
Ismaila Hassan <code.ismailah28@gmail.com>
2022-07-12 11:51:23 -0700

    add function to print out string

---
+++ _print_str.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_str - function that prints a string to standard output
+ * @str: pointer to string to print
+ * Return: length of string
+ */
+int _print_str(char *str)
+{
+	int count = 0;
+
+	if (str == NULL)
+		return (0);
+	while (*str)
+	{
+		_putchar(*str++);
+		count++;
+	}
+	return (count);
+}

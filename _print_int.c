2312c0cee3d55cdf15d2661d1895fc1996e2d936
Ismaila Hassan <code.ismailah28@gmail.com>
2022-07-12 11:52:18 -0700

    add function to print out int

---
+++ _print_int.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_int - prints an integer
+ * @var: variable to be printed
+ * Return: length of printed characters
+ */
+
+int _print_int(long int num)
+{
+	int count = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		count += 1;
+		num = -num;
+	}
+	if (num / 10)
+		count += _print_int(num / 10);
+
+
+	_putchar(num % 10 + '0');
+	count += 1;
+	return (count);
+}
